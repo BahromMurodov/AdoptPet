97376b843b8e41dc1492252373f4067cd59a949b
bahrommurodov767@gmail.com <bahrommurodov767@gmail.com>
2024-08-10 00:03:47 +0500

    class PetPhoto

---
+++ backend/src/AdoptPet.Domain/Entities/PetPhoto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoptPet.Domain.Entities;
+
+public class PetPhoto
+{
+    public Guid Id { get; private set; }
+    public string Path { get; private set; } = string.Empty!;
+    public bool IsMain { get; private set; }
+}
