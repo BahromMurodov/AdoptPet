ce60d9ceb1834ffb08fb23e5e9fd627cc241c794
bahrommurodov767@gmail.com <bahrommurodov767@gmail.com>
2024-08-09 23:47:48 +0500

    create model Volunteer

---
+++ backend/src/AdoptPet.Domain/Entities/SocialMedia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoptPet.Domain.Entities;
+
+public class SocialMedia
+{
+    public string Link { get; private set; } = string.Empty!;
+    public string Name { get; private set; } = string.Empty!;
+}
---
+++ backend/src/AdoptPet.Domain/Entities/Volunteer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoptPet.Domain.Entities;
+
+public class Volunteer
+{
+    public Guid Id { get; private set; }
+    public string FullName { get; private set; } = string.Empty!;
+    public string Description { get; private set; } = string.Empty!;
+    public int Experience { get; private set; }
+    public int NumberPetFindHome { get; private set; }
+    public int NumberPetSearchHome { get; private set; }
+    public int NumberPetHealing { get; private set; }
+    public string Phone { get; private set; } = string.Empty!;
+    public List<SocialMedia> SocialMedias { get; private set; } = null!;
+    public List<Requisite> Requisites { get; private set; } = null!;
+    public List<Pet> Pets { get; private set; } = null!;
+
+    private Volunteer()
+    {
+    }
+}
