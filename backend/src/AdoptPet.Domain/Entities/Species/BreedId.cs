2b2513d0edeb48f565d0f9ec0f5d436d6865c518
bahrommurodov767@gmail.com <bahrommurodov767@gmail.com>
2024-08-23 17:30:11 +0500

    breed species fixed(убрал) nullable

--- backend/src/AdoptPet.Domain/Entities/Species/BreedId.cs
+++ backend/src/AdoptPet.Domain/Entities/Species/BreedId.cs
@@ -2,24 +2,24 @@
 
 public record BreedId
 {
-    public BreedId(Guid? value)
+    public BreedId(Guid value)
     {
         Value = value;
     }
 
-    public Guid? Value { get; }
+    public Guid Value { get; }
     public static BreedId NewBreedId() => Guid.NewGuid();
 
     public static BreedId Empty() => Guid.Empty;
 
-    public static BreedId Create(Guid? id) => new(id);
+    public static BreedId Create(Guid id) => new(id);
 
-    public static implicit operator BreedId(Guid? id) => new(id);
+    public static implicit operator BreedId(Guid id) => new(id);
 
     public static implicit operator Guid(BreedId BreedId)
     {
         ArgumentNullException.ThrowIfNull(BreedId);
         ArgumentNullException.ThrowIfNull(BreedId.Value);
-        return BreedId.Value.Value;
+        return BreedId.Value;
     }
 }
